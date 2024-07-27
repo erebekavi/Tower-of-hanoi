89c14c7d84b95a1d36241f4f23355e5285d42515
kavi <nithveenkavi.2006@gmail.com>
2024-07-27 18:41:42 +0530

    variable changed fixed

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def towerofhanoi (n,sa,sb,sc):
-    if n == 0:
+def towerofhanoi (n,a,b,c):
+    if n == 1:
+        print(f"move 1st disk from rod {a} to rod {c}\n")
         return
-    towerofhanoi(n-1,sa,sc,sb)
-    print(f"move disk {n} from rod {sa} to rod {sc}\n")
-    towerofhanoi(n-1,sc,sb,sa)
+    towerofhanoi(n-1,a,c,b)
+    print(f"move disk {n} from rod {a} to rod {c}\n")
+    towerofhanoi(n-1,b,a,c)
     
 towerofhanoi(3,"a","b","c")
